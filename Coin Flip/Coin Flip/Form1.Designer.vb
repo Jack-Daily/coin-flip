89dd79b00961421a3811ce4cfe182c6d9e648e5d
Jack-Daily <jd306127@students.davenport.k12.ia.us>
2016-09-29 11:17:06 -0500

    Add files via upload

---
+++ Coin Flip/Coin Flip/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnFlip = New System.Windows.Forms.Button()
+        Me.picHeads = New System.Windows.Forms.PictureBox()
+        Me.picTails = New System.Windows.Forms.PictureBox()
+        CType(Me.picHeads, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picTails, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(12, 331)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(92, 32)
+        Me.btnExit.TabIndex = 0
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnFlip
+        '
+        Me.btnFlip.Location = New System.Drawing.Point(191, 331)
+        Me.btnFlip.Name = "btnFlip"
+        Me.btnFlip.Size = New System.Drawing.Size(92, 32)
+        Me.btnFlip.TabIndex = 5
+        Me.btnFlip.Text = "Flip Coin"
+        Me.btnFlip.UseVisualStyleBackColor = True
+        '
+        'picHeads
+        '
+        Me.picHeads.Image = CType(resources.GetObject("picHeads.Image"), System.Drawing.Image)
+        Me.picHeads.Location = New System.Drawing.Point(12, 12)
+        Me.picHeads.Name = "picHeads"
+        Me.picHeads.Size = New System.Drawing.Size(224, 225)
+        Me.picHeads.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.picHeads.TabIndex = 7
+        Me.picHeads.TabStop = False
+        '
+        'picTails
+        '
+        Me.picTails.Image = CType(resources.GetObject("picTails.Image"), System.Drawing.Image)
+        Me.picTails.Location = New System.Drawing.Point(270, 12)
+        Me.picTails.Name = "picTails"
+        Me.picTails.Size = New System.Drawing.Size(225, 224)
+        Me.picTails.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.picTails.TabIndex = 8
+        Me.picTails.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(507, 375)
+        Me.Controls.Add(Me.picTails)
+        Me.Controls.Add(Me.picHeads)
+        Me.Controls.Add(Me.btnFlip)
+        Me.Controls.Add(Me.btnExit)
+        Me.Name = "Form1"
+        Me.Text = "Coin Toss"
+        CType(Me.picHeads, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picTails, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnFlip As System.Windows.Forms.Button
+    Friend WithEvents picHeads As System.Windows.Forms.PictureBox
+    Friend WithEvents picTails As System.Windows.Forms.PictureBox
+
+End Class
